8d3731d2c26952976d1a112895c9f341831f2d1e
Xander1230 <47845282+Xander1230@users.noreply.github.com>
2019-02-21 01:59:35 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld { //Header of the class
+	//usually instance variables go here
+static int x=5;
+static double y=2.5;
+static double z=4;
+static String a="Hello";
+static String b="Goodbye";
+	//My main method
+public static void main(String[] args) { //this is the header of a method... main method
+		// TODO Auto-generated method stub
+System.out.print(a+"\n");
+System.out.print(b+"\t");
+System.out.print("a"+"\t");
+z=x/y+x*y-x;
+System.out.print(z);
+	}
+
+}
